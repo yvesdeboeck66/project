54b29138484f800b653eaec5adfbea09dee4bb7a
gauthierdb <36762689+gauthierdb@users.noreply.github.com>
2019-11-26 17:06:28 +0100

    Updated databaseFill

--- pyd7a-master/userCode/DatabaseFill.py
+++ pyd7a-master/userCode/DatabaseFill.py
@@ -1,12 +1,11 @@
 
 import paho.mqtt.subscribe as subscribe
-import time
 import paho.mqtt.publish as publish
 import time, datetime
 import paho.mqtt.client as mqttclient
-import pymongo
 from d7a.alp.parser import Parser as AlpParser
 from bitstring import ConstBitStream
+import pymongo
 
 import logging
 
@@ -32,61 +31,88 @@ def gateway_name(param):
     if param == "4337313400210032":
         return "Gateway4"
 
-flag=False
-start=0
-counter = 0
-initTime = datetime.datetime.now()
-
-def on_message(client, userdata, message):
-    print("in onmessage")
-    global flag,start, measurement, counter, initTime
-    if flag==False :
-        start = time.time()
-    flag=True
-
-    # If the time difference from the initial message has not exceeded 2 seconds, check if message comes in
-    # If after 2 seconds the counter is not 3, do 'IncorrectMeasurement' function with data you have
-    # If after 2 seconds the counter is 3, send to mongoDB
-    timeDiff = initTime.timeDelta
-
-
-    print("NEW MESSAGE")
-    dataByteArray = bytearray(message.payload.decode("hex"))
-    payload = AlpParser().parse(ConstBitStream(dataByteArray), len(dataByteArray))
-    gateway_id = message.topic.split("/")[3]
-    gw_name = gateway_name(gateway_id)
-    gw_access_id = gateway_access_id(gateway_id)
-    payload_data = payload.actions[0].operand.data
-    payload_linkBudget = payload.interface_status.operation.operand.interface_status.link_budget
-    print("Gateway : "+ gw_name+" Link budget: "+ str(payload_linkBudget))
+def gateway_index(param):
     cases={"Gateway1" : 0, "Gateway2" : 1, "Gateway3" : 2, "Gateway4": 3}
-    measurement[cases.get(gw_name)] = payload_linkBudget
+    return cases.get(param)
 
-def WriteMongo():
-    for i in measurement:
-        if(i==0):
-            i=-1000
-    location=input("Give location plz")
-    dictmeasurement={"RSSI":measurement,"location":location}
+def sendToDatabase(param):
+
+    #Fix the unknown data points
+    for i in range(len(param)):
+        if(param[i] == 0):
+            param[i] = -1000
+
+    #Get a location for the database
+    location = input("What location did you test?")
+
+    #make the entry & send it to the database
+    dictmeasurement = {"RSSI": param, "location": location}
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")  # Connect met database
     mydb = myclient["IOT"]
     mycol = mydb["TrainingDatabase"]
     mycol.insert_one(dictmeasurement)  # {} --> eerste doc in collection, measurementset is een list van docs eigenlijk dus neem de 1ste doc uit de lijst
-    return 0
+    #To do: Check if dictmeasurement is created as intended
+
+msgCounter = 0
+initMinute = 0
+measurement = [0,0,0,0]
+
+def on_message(client, userdata, message):
+    global msgCounter, initMinute, measurement
+    msgCounter = msgCounter + 1
 
 
-print("begin")
-measurement=[0,0,0,0]
+    dataByteArray = bytearray(message.payload.decode("hex"))
+    payload = AlpParser().parse(ConstBitStream(dataByteArray), len(dataByteArray))
+
+    gateway_id = message.topic.split("/")[3]
+    gw_name = gateway_name(gateway_id)
+    gw_access_id = gateway_access_id(gateway_id)
+
+
+    payload_data = payload.actions[0].operand.data
+    payload_linkBudget = payload.interface_status.operation.operand.interface_status.link_budget
+    print("Message from " + gw_name)
+    print("Link budget: "+ str(payload_linkBudget) + "\n")
+
+
+    # TIME
+    then = datetime.datetime.now()
+    timeStamp = str(time.mktime(then.timetuple())*1e3 + then.microsecond/1e3)
+    currentMinute = then.minute
+
+    # We check if message have arrived in all four gateways by giving the broadcast a 2-second TTL
+    # If the time difference from the initial message has not exceeded 2 seconds, check if message comes in
+    # If after 2 seconds the counter is not 3, do 'IncorrectMeasurement' function with data you have
+    # If after 2 seconds the counter is 3, send to mongoDB
+
+    measurement[gateway_index(gw_name)] = payload_linkBudget
+
+    # Debug#
+    print(currentMinute - initMinute)
+
+    if msgCounter <= 1:
+        initMinute = currentMinute
+        msgCounter = msgCounter + 1
+
+    elif msgCounter < 4 & (currentMinute - initMinute) < 2:
+        msgCounter = msgCounter + 1
+        if msgCounter == 4 :
+            sendToDatabase(measurement)
+            msgCounter = 0
+            measurement = [0,0,0,0]
+
+    else:
+        sendToDatabase(measurement)
+        msgCounter = 0
+        measurement = [0,0,0,0]
+
+
 subscribe.callback(on_message, "/d7/4836383700470033/#", hostname="student-04.idlab.uantwerpen.be")
 
 
-print("na callback")
-while(1):
-    print("In de while loop")
-    end = time.time()
-    if(end-start>3):
-        start=0
-        flag= False
-        WriteMongo()
-        measurement = [0, 0, 0, 0]
+
+#client1 = mqttclient.Client("student03server")
+#client1.connect("http://student-03.idlab.uantwerpen.be", 8080, keepalive=600)
+
 
